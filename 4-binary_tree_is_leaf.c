6c4397f87aaea4257eee4af620a1d557bc82c81f
Juan Jose <1561@holbertonschool.com>
2020-07-21 22:25:20 +0000

    [add] first version

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - will check if node is a leaf
+ * @node: variable
+ * Return: 1 or 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
