35e23cc8ac829c8b77951c75e58cb3bee27031ca
Juan Jose <1561@holbertonschool.com>
2020-07-22 16:53:45 +0000

    [add] first version

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - will return the ize of a binary tree
+ * @tree: variable
+ * Return: 1 or 0
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	return (binary_tree_size(tree->left) + 1
+		+ binary_tree_size(tree->right));
+}
